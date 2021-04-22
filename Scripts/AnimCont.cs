46bddfd4c52753137aeddc2706f0d4d8ae062792
Codes109 <68227390+Codes109@users.noreply.github.com>
2021-04-22 19:01:11 +1000

    Moved scripts to folder.

---
+++ Scripts/AnimCont.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimCont : MonoBehaviour
+{
+    public Animator playerAnim;
+        
+        private void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            playerAnim.SetBool("run", true);
+        }
+        else
+        {
+            playerAnim.SetBool("run", false);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerAnim.SetBool("attack", true);
+            playerAnim.SetBool("canMove", false);
+        }
+        else
+        {
+            
+            playerAnim.SetBool("attack", false);
+            playerAnim.SetBool("canMove", true);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            playerAnim.SetBool("block", true);
+            playerAnim.SetBool("canMove", false);
+        }
+        else
+        {
+
+            playerAnim.SetBool("block", false);
+            playerAnim.SetBool("canMove", true);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            playerAnim.SetBool("roll", true);
+        }
+        else
+        {
+
+            playerAnim.SetBool("roll", false);
+        }
+    }
+
+
+}
